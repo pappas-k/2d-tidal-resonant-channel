e7dd1af7ce762656d2e8b5d3eec98282a8cfc614
pappas-k <pappas-k@users.noreply.github.com>
2026-05-01 18:51:34 +0300

    Fix spacing in amplification_analysis.py
    Remove extra blank line and add spaces around assignment operators to
    follow PEP 8 style.

--- amplification_analysis.py
+++ amplification_analysis.py
@@ -32,20 +32,19 @@ rc('font', **{'family': 'sans-serif', 'sans-serif': ['Helvetica'], 'size': 18})
 rc('text', usetex=True)
 
 
-
-file= "min_max.csv"    #read the min_max.csv with the maximum and minimum values of elevation eta
-df=pd.read_csv(file, names=['index', 'xcoord', 'eta_max', 'eta_min'] )   #convert it to a dataframe
+file = "min_max.csv"
+df = pd.read_csv(file, names=['index', 'xcoord', 'eta_max', 'eta_min'])
 print(df)
 
-lx=180000
+lx = 180000
 H_cases = [45,47.5,48.5,49.5,50,52.5,55]
 H_labels = ['H=45 m','H=47.5 m','H=48.5 m','H=49.5 m','H=50 m','H=52.5 m','H=55 m']
 print(df['eta_max'][0:10])
 colors = ["red", "blue" , "green", "orange", "purple", "k", "lightseagreen"]
 linewidth = 0.8
 
-for j in np.arange(0,len(H_cases)):
-    i=j*21
+for j in np.arange(0, len(H_cases)):
+    i = j * 21
     plt.plot(df['xcoord'][0:21] / lx, df['eta_max'][i:i+21], label=H_labels[j], color=colors[j], lw=linewidth)
     plt.plot(df['xcoord'][0:21] / lx, df['eta_min'][i:i + 21], color=colors[j], lw=linewidth)
 

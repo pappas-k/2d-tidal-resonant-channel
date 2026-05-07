c4ed9fbecd4f900fc5cf8de33275caa2cbb7cd45
pappas-k <pappas.k@outlook.com>
2026-05-07 18:55:03 +0300

    Use context manager for mesh.geo file write in gmsh_generator.py
    Replace the manual open/close pattern with a with statement so the
    file is always closed safely, and normalise spacing around range().

--- gmsh_generator.py
+++ gmsh_generator.py
@@ -19,31 +19,28 @@ def gmsh_generator(outline,resolution):
     os.chdir("mesh")
     call(["rm", "mesh.msh"])
 
-    f1 = open("mesh.geo","w")
+    with open("mesh.geo", "w") as f:
+        for i in range(len(outline)):
+            f.write('Point(' + str(i+1) + ') = { ' + "{}, {}, 0, {}".format(outline[i][0], outline[i][1], resolution[i]) + "}; \n")
 
-    for i in range (len(outline)):
-        f1.write('Point('+str(i+1)+') = { ' +"{}, {}, 0, {}".format(outline[i][0], outline[i][1],resolution[i]) + "}; \n")
+        for i in range(len(outline)-1):
+            f.write('Line(' + str(i+1) + ') = { ' + "{}, {}".format(len(outline)-i, len(outline)-1-i) + "}; \n")
 
-    for i in range (len(outline)-1):
-        f1.write('Line('+str(i+1)+') = { ' +"{}, {}".format(len(outline)-i, len(outline)-1-i,) + "}; \n")
+        # Final connection
+        f.write('Line(' + str(len(outline)) + ') = { ' + "{}, {}".format(1, len(outline)) + "}; \n")
+        f.write('Line Loop(1) = {')
+        for i in range(len(outline)):
+            f.write(str(i+1))
+            if i < len(outline)-1:
+                f.write(", ")
+        f.write('};\n')
 
-    # Final connection
-    f1.write('Line('+str(len(outline))+') = { ' +"{}, {}".format(1 , len(outline),) + "}; \n")
-    f1.write('Line Loop(1) = {')
-    for i in range (len(outline)):
-        f1.write(str(i+1))
-        if i < len(outline)-1:
-            f1.write(", ")
-    f1.write('};\n')
+        f.write('Plane Surface(6) = {1};\n')
+        for i in range(len(outline)):
+            f.write('Physical Line(' + str(i+1) + ') = { ' + "{}".format(i+1) + "}; \n")
 
-    f1.write('Plane Surface(6) = {1};\n')
-    for i in range (len(outline)):
-        f1.write('Physical Line('+str(i+1)+') = { '+"{}".format(i+1) + "}; \n")
-
-    f1.write('Physical Surface(11) = {6};\n' )
-    f1.write('Mesh.Algorithm = 6; // frontal=6, delannay=5, meshadapt=1')
-
-    f1.close()
+        f.write('Physical Surface(11) = {6};\n')
+        f.write('Mesh.Algorithm = 6; // frontal=6, delannay=5, meshadapt=1')
 
 lx = 30000  # length of channel
 lw = 2000   # width of channel
